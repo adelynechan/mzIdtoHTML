f6da66717f2124ec4f8a194e87bb96174176b65c
adelynechan <adelynechan@gmail.com>
2015-05-08 15:02:50 +0100

    work on metadata and start building peptide view

--- mzIdtoHTML/src/main/java/javaapplication2/metadata.java
+++ mzIdtoHTML/src/main/java/javaapplication2/metadata.java
@@ -6,103 +6,122 @@
 package javaapplication2;
 
 import java.io.File;
-import org.apache.commons.io.FileUtils;
 
 import uk.ac.ebi.jmzidml.model.mzidml.*;
 import uk.ac.ebi.jmzidml.xml.io.MzIdentMLUnmarshaller;
 
 import java.util.List;
-//import java.lang.StringBuilder;
 
 /**
  *
  * @author Adelyne
  */
-public class metadata {
-
-    public static void main(String[] args) {
-
-        try {
-
-            File file = new File("GalaxyExampleProteoGrouper.mzid");
+public class Metadata {
+    
+    public static String getSearchType (File file) { 
+        MzIdentMLUnmarshaller unmarshaller = new MzIdentMLUnmarshaller(file);
+        SpectrumIdentificationProtocol sip = unmarshaller.unmarshal(SpectrumIdentificationProtocol.class);
             
-            if (file.exists()) {
-
-                boolean aUseSpectrumCache = true;
-                MzIdentMLUnmarshaller unmarshaller = new MzIdentMLUnmarshaller(file);
-
-                SpectrumIdentificationProtocol sip = unmarshaller.unmarshal(SpectrumIdentificationProtocol.class);
-                AnalysisSoftware as = unmarshaller.unmarshal(AnalysisSoftware.class);
-
-                // Extract search type information
-                Param searchType = sip.getSearchType();
-                CvParam searchCv = searchType.getCvParam();
-                String searchString = searchCv.getName();
-
-                // Extract software name information
-                Param softwareType = as.getSoftwareName();
-                CvParam softwareCv = softwareType.getCvParam();
-                String softwareString = softwareCv.getName();
-
-                // Extract enzymes as a list
-                Enzymes enzymes = sip.getEnzymes();
-                List<Enzyme> enzList = enzymes.getEnzyme();
-
-                // Extract search modifications as a list
-                ModificationParams mp = sip.getModificationParams();
-                List<SearchModification> modList = mp.getSearchModification();
-                
-                // Print out extracted metadata to output file
-                // Based on HTML template
-                String printSearchType = "Type of Search: " + searchString;
-                String printSoftwareName = "Analysis Software: " + softwareString;
-                
-                StringBuilder enzymeBuilder = new StringBuilder();
-
-                for (Enzyme enzymeList : enzList) {
-                    ParamList enzymeName = enzymeList.getEnzymeName();
-                    List<CvParam> enzymeParam;
-                    enzymeParam = enzymeName.getCvParam();
-
-                    for (int i = 0; i < enzymeParam.size(); i++) {
-                        enzymeBuilder.append(enzymeParam.get(i).getName());
-                    }
-                }
-
-                String printEnzymes = "Enzymes: " + enzymeBuilder.toString();
-                                
-                StringBuilder modificationsBuilder = new StringBuilder();
-                
-                if (modList.isEmpty()) {
-                    modificationsBuilder.append("No modifications");
-                }
-               
-                else {
-                    for (SearchModification modificationList : modList) {
-                    List<String> modificationName = modificationList.getResidues();
-                    
-                    for (int i = 0; i < modificationName.size(); i++) {
-                            modificationsBuilder.append(modificationName.get(i));
-                            modificationsBuilder.append(modificationName.size());
-                        }                                              
-                    }
-                }
-                String printModifications = "Modifications: " + modificationsBuilder.toString();
-                                               
-                File htmlTemplateFile = new File("template.html");
-                String htmlString = FileUtils.readFileToString(htmlTemplateFile);
-                
-                htmlString = htmlString.replace("$searchtype", printSearchType);
-                htmlString = htmlString.replace("$softwarename", printSoftwareName);
-                htmlString = htmlString.replace("$enzymes", printEnzymes);
-                htmlString = htmlString.replace("$modifications", printModifications);
-                File newHtmlFile = new File("template1.html");
-                FileUtils.writeStringToFile(newHtmlFile, htmlString);
-                }
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
+        Param searchTypeParam = sip.getSearchType();
+        CvParam searchCv = searchTypeParam.getCvParam();
+        String searchType = searchCv.getName();  
+            
+        return searchType;
     }
+        
+    public static String getSoftwareName (File file) {
+        MzIdentMLUnmarshaller unmarshaller = new MzIdentMLUnmarshaller(file);
+        AnalysisSoftware as = unmarshaller.unmarshal(AnalysisSoftware.class);
+        
+        Param softwareType = as.getSoftwareName();
+        CvParam softwareCv = softwareType.getCvParam();
+        String softwareName = softwareCv.getName();
+        
+        return softwareName;
+    }   
+    
+    public static String getEnzymesUsed (File file) {
+        MzIdentMLUnmarshaller unmarshaller = new MzIdentMLUnmarshaller(file);
+        SpectrumIdentificationProtocol sip = unmarshaller.unmarshal(SpectrumIdentificationProtocol.class);
+        Enzymes enzymes = sip.getEnzymes();
+        
+        List<Enzyme> enzList = enzymes.getEnzyme();
+                
+        StringBuilder enzymeBuilder = new StringBuilder();              
+        
+        if (enzList.isEmpty()) {
+            enzymeBuilder.append("Information not available");
+        }
+       
+        else {
+            for (Enzyme enzymeList : enzList) {
+                ParamList enzymeName = enzymeList.getEnzymeName();
+                List<CvParam> enzymeParam;
+                enzymeParam = enzymeName.getCvParam();
 
-}
+                for (int i = 0; i < enzymeParam.size(); i++) {
+                    enzymeBuilder = enzymeBuilder.append(enzymeParam.get(i).getName()); 
+                }
+            }
+        }
+        
+        return enzymeBuilder.toString();
+    }   
+       
+    public static String getFixedModifications (File file) {
+        MzIdentMLUnmarshaller unmarshaller = new MzIdentMLUnmarshaller(file);
+        SpectrumIdentificationProtocol sip = unmarshaller.unmarshal(SpectrumIdentificationProtocol.class);
+        ModificationParams mp = sip.getModificationParams();
+        List<SearchModification> modList = mp.getSearchModification();       
+             
+        StringBuilder fixedModificationsBuilder = new StringBuilder();
+                        
+        if (modList.isEmpty()) {
+            fixedModificationsBuilder.append("No modifications");
+        }
+                       
+        else {
+                        
+            for (SearchModification modificationList : modList) {
+                Boolean fixed = modificationList.isFixedMod();
+                
+                if (fixed) {
+                    List <String> modificationName = modificationList.getResidues();
+                    for (int i = 0; i < modificationName.size(); i++) {
+                    fixedModificationsBuilder.append(modificationName.get(i));
+                    }                                              
+                }
+            }
+        }
+        return fixedModificationsBuilder.toString();
+    }
+    
+    public static String getVariableModifications (File file) {
+        MzIdentMLUnmarshaller unmarshaller = new MzIdentMLUnmarshaller(file);
+        SpectrumIdentificationProtocol sip = unmarshaller.unmarshal(SpectrumIdentificationProtocol.class);
+        ModificationParams mp = sip.getModificationParams();
+        List<SearchModification> modList = mp.getSearchModification();       
+             
+        StringBuilder variableModificationsBuilder = new StringBuilder();
+                        
+        if (modList.isEmpty()) {
+            variableModificationsBuilder.append("No modifications");
+        }
+                       
+        else {
+                        
+            for (SearchModification modificationList : modList) {
+                Boolean fixed = modificationList.isFixedMod();
+                
+                if (!fixed) {
+                    List <String> modificationName = modificationList.getResidues();
+                    for (int i = 0; i < modificationName.size(); i++) {
+                    variableModificationsBuilder.append(modificationName.get(i));
+                    }                                              
+                }
+            }
+        }
+        return variableModificationsBuilder.toString();
+    }
+      
 }
--- mzIdtoHTML/src/main/java/javaapplication2/peptideview.java
+++ mzIdtoHTML/src/main/java/javaapplication2/peptideview.java
@@ -5,10 +5,74 @@
  */
 package javaapplication2;
 
+import java.io.File;
+import java.util.List;
+import uk.ac.ebi.jmzidml.MzIdentMLElement;
+import uk.ac.ebi.jmzidml.model.mzidml.AnalysisData;
+import uk.ac.ebi.jmzidml.model.mzidml.DataCollection;
+import uk.ac.ebi.jmzidml.model.mzidml.Peptide;
+import uk.ac.ebi.jmzidml.model.mzidml.SpectrumIdentificationItem;
+import uk.ac.ebi.jmzidml.model.mzidml.SpectrumIdentificationList;
+import uk.ac.ebi.jmzidml.model.mzidml.SpectrumIdentificationResult;
+import uk.ac.ebi.jmzidml.xml.io.MzIdentMLUnmarshaller;
+
 /**
  *
  * @author Adelyne
  */
 public class peptideview {
     
+    public static String getPeptideInfo (File file) {
+               
+        boolean aUseSpectrumCache = true;
+        MzIdentMLUnmarshaller unmarshaller = new MzIdentMLUnmarshaller(file);
+                                
+        DataCollection dc =  unmarshaller.unmarshal(DataCollection.class);
+        AnalysisData ad = dc.getAnalysisData();
+                
+        StringBuilder peptideInfoBuilder = new StringBuilder();
+
+        // Get the list of SpectrumIdentification elements
+        List<SpectrumIdentificationList> sil = ad.getSpectrumIdentificationList();
+
+        for (SpectrumIdentificationList sIdentList : sil) {
+            for (SpectrumIdentificationResult spectrumIdentResult: sIdentList.getSpectrumIdentificationResult()) {
+
+                // Get the name of SpectrumIdentificationResult
+                String spectrumID =  spectrumIdentResult.getSpectrumID();
+
+                for (SpectrumIdentificationItem spectrumIdentItem: spectrumIdentResult.getSpectrumIdentificationItem()) {
+
+                    // Get the following information for SpectrumIdentificationItem element
+                    String spectrumIdItem = spectrumIdentItem.getId();
+                    Double calculatedMassToCharge =  spectrumIdentItem.getCalculatedMassToCharge();
+                    Double experimentalMassToCharge = spectrumIdentItem.getExperimentalMassToCharge();
+                    int charge = spectrumIdentItem.getChargeState();
+                    
+                    String printSpectrumIdItem = "<td> " + spectrumIdItem + " </td>";
+                    String printSequence = "<td> " + " " + " </td>";
+                    String printCalculatedMassToCharge = "<td> " + calculatedMassToCharge + " </td>";
+                    String printExperimentalMassToCharge = "<td> " + experimentalMassToCharge + " </td>";
+                    String printCharge = "<td> " + charge + " </td>";
+                    String printModifications = "<td> " + " " + " </td>";
+                    String printScore = "<td> " + " " + " </td>";
+                    String printAssociatedProteins = "<td> " + " " + " </td>";
+                    
+                    peptideInfoBuilder.append("<tr>");
+                    peptideInfoBuilder.append(printSpectrumIdItem);
+                    peptideInfoBuilder.append(printSequence); // for sequence
+                    peptideInfoBuilder.append(printCalculatedMassToCharge);
+                    peptideInfoBuilder.append(printExperimentalMassToCharge);
+                    peptideInfoBuilder.append(printCharge);
+                    peptideInfoBuilder.append(printModifications);
+                    peptideInfoBuilder.append(printScore);
+                    peptideInfoBuilder.append(printAssociatedProteins);
+                    peptideInfoBuilder.append("</tr>");
+
+                }                                                                                  
+            }
+        }  
+                                    
+        return peptideInfoBuilder.toString();
+    }         
 }
